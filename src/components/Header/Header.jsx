30614f83dfbd1bc7a073e3372a65f8660e80fcbb
pereriafernando13 <nando-psousa@outlook.com>
2025-10-14 12:26:22 +0100

    add dependencias, prettier, eslint and start do header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div className="header__logo"> Fiore dentro di Me 🌸</div>
+      <nav className="header__nav">
+        <a href="">Home</a>
+        <a href="">About us</a>
+        <a href="">Purpose</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
