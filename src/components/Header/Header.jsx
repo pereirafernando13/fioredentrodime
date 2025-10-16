05a8ac5b0f0bc052bf52ba01abda2741d38e1405
pereriafernando13 <nando-psousa@outlook.com>
2025-10-16 13:16:33 +0100

    add header/style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,21 @@ import React from "react";
 
 function Header() {
   return (
-    <header>
+    <header className="header">
       <div className="header__logo"> Fiore dentro di Me 🌸</div>
       <nav className="header__nav">
-        <a href="">Home</a>
-        <a href="">About us</a>
-        <a href="">Purpose</a>
+        <a className="header__nav-link" href="">
+          Home
+        </a>
+        <a className="header__nav-link" href="">
+          Gallery
+        </a>
+        <a className="header__nav-link" href="">
+          Purpose
+        </a>
+        <a className="header__nav-link" href="">
+          About us
+        </a>
       </nav>
     </header>
   );
