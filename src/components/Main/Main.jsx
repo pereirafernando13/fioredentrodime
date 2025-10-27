143f20bb467871ad2fd60839c92cab18fb70be01
pereriafernando13 <nando-psousa@outlook.com>
2025-10-27 18:18:49 +0000

    add componente main, cover principal

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Main() {
+  return (
+    <>
+      <section className="cover">
+        <img className="cover__img" src="/cover.jpg" alt="" />
+        <div className="cover__content">
+          <h1 className="cover__content-title">Fiore is...</h1>
+          <h2 className="cover__content-subtitle">
+            the path, the flower, the feeling.
+          </h2>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Main;
